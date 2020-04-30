7ee0005a3b23d2161985a58c17ab8d108a17a7c8
alokshakya <alokshakyacode@gmail.com>
2020-04-30 14:29:53 +0530

    login UI rendered

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-
+import { FormsModule, ReactiveFormsModule}  from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +12,9 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
