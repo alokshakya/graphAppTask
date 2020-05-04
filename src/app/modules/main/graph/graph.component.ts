c719c73c12330f742cf901d56ca887fc3f95f614
alokshakya <alokshakyacode@gmail.com>
2020-05-04 18:36:23 +0530

    graphs pie done

--- src/app/modules/main/graph/graph.component.ts
+++ src/app/modules/main/graph/graph.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { ReplaySubject } from 'rxjs';
+import { takeUntil } from 'rxjs/internal/operators';
+import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-graph',
   templateUrl: './graph.component.html',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private auth:AuthService, private fb:FormBuilder) { }
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
   ngOnInit(): void {
+    this.initInfoForm();
+    //this.infoForm.get('date').setValue(this.currentDate());
   }
+
+  graphTypes=["Pie", "Bar"];
+
+  infoForm:FormGroup;
+  initInfoForm() {
+    this.infoForm = this.fb.group({
+      name: ['',[Validators.required]],
+      type: [this.graphTypes[0]],
+      date:['']
+    });
+    this.infoForm.valueChanges.pipe(takeUntil(this.destroyed$)).subscribe( (r) =>{
+      
+    })
+    
+  }
+  get filename(){
+    return this.infoForm.get('name').value;
+  }
+  get gType(){
+    return this.infoForm.get('type').value;
+  }
+  get controls(){
+    return this.infoForm.controls;
+  }
+
+  setDate(event){
+    this.infoForm.get('date').setValue(event);
+  }
+  currentDate() {
+    const currentDate = new Date();
+    return currentDate.toISOString().substring(0,10);
+  }
+
+  ngOnDestroy(){
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
+  }
+
 
 }
--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { GraphComponent } from './graph/graph.component';
 
 import { MainRoutingModule } from './main-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DisplayInformationComponent } from './display-information/display-information.component';
 
 @NgModule({
-  declarations: [HomeComponent, GraphComponent, DashboardComponent],
+  declarations: [HomeComponent, GraphComponent, DashboardComponent, DisplayInformationComponent],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class MainModule { }
